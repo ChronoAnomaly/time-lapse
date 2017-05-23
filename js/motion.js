ffcbf1638ed8f1ab5bde34c9486ae22932385c94
ChronoAnomaly <chronoanomaly@gmail.com>
2017-05-23 13:27:16 -0400

    Inital work for motion testing

---
+++ js/motion.js
@@ -0,0 +1,60 @@
+var constraints = {
+  audio: false,
+  video: {
+    width: 640,
+    height: 480
+  }
+};
+navigator.mediaDevices.getUserMedia(constraints).then(success).catch(error);
+
+function success(stream) {
+  var video = document.getElementById('video');
+  video.srcObject = stream;
+}
+
+function error(error) {
+  console.log(error);
+}
+
+
+
+var canvas = document.getElementById('motion');
+canvas.width = 640;
+canvas.height = 480;
+var context = canvas.getContext('2d');
+context.globalCompositeOperation = 'difference';
+
+
+setInterval(capture, 100);
+
+function capture() {
+  // context.drawImage(video, 640, 480); // testing for viewing current canvas
+
+  context.drawImage(video, 0, 0, 640, 480);
+
+  // do other stuff
+
+}
+console.log(canvas);
+  var imageData = canvas.getImageData();
+  var imageScore = 0;
+
+  for (var i = 0; i < imageData.data.length; i += 4) {
+    var r = imageData.data[i] / 3;
+    var g = imageData.data[i + 1] / 3;
+    var b = imageData.data[i + 2] / 3;
+    var pixelScore = r + g + b;
+
+    if (pixelScore >= PIXEL_SCORE_THRESHOLD) {
+      imageScore++;
+    }
+  }
+
+  if (imageScore >= IMAGE_SCORE_THRESHOLD) {
+    // we have motion!
+  }
+navigator.mediaDevices.enumerateDevices().then(function(devices) {
+  devices.forEach(function(device) {
+    $('#tester').append("<p>" + device.kind + ": " + device.label + " id = " + device.deviceId + "</p>");
+  });
+})
